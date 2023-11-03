1565ba894e3108c329102ae4e3058fb79dad389d
Zachary Pangerl <zpangerl@asu.edu>
2023-11-03 16:17:31 -0700

    US87-Task124: Add BeltRank class

---
+++ src/main/java/memoranda/BeltRank.java
@@ -0,0 +1,21 @@
+package main.java.memoranda;
+
+public class BeltRank {
+    
+    public enum Rank {
+        WHITE,
+        YELLOW,
+        ORANGE,
+        PURPLE,
+        BLUE,
+        BLUE_STRIPE,
+        GREEN,
+        GREEN_STRIPE,
+        BROWN1,
+        BROWN2,
+        BROWN3,
+        BLACK1,
+        BLACK2,
+        BLACK3
+    }
+}
