4e1051be736f3f2e32047c3e4d2b6e0c9881f1f8
Sean <smckeig1@asu.edu>
2023-11-03 10:09:51 -0500

    US79/Task103, AddStudentDialog test launch class

--- src/test/java/dialogTest_StudentAddDialog.java
+++ src/test/java/dialogTest_StudentAddDialog.java
@@ -1,12 +1,16 @@
 import main.java.memoranda.ui.AddStudentDialog;
 import main.java.memoranda.ui.App;
 import main.java.memoranda.ui.TaskDialog;
+import main.java.memoranda.ui.TaskTable;
 import main.java.memoranda.util.Local;
+
+import java.awt.*;
 
 public class dialogTest_StudentAddDialog {
 
     public static void main(String args[]){
-        AddStudentDialog dlg = new AddStudentDialog(App.getFrame(), Local.getString("New Student"));
+        AddStudentDialog dlg = new AddStudentDialog(App.getFrame(), "New Student");
+        dlg.setVisible(true);
     }
 
 }
