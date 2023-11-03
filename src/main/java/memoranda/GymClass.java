74d4169a438f006b1d4609515c74dee7b4b5e625
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-11-02 23:07:58 -0700

    US83-T129: Add GymClass.java

---
+++ src/main/java/memoranda/GymClass.java
@@ -0,0 +1,18 @@
+/**
+ * GymClass.java
+ * Created: Nov. 2nd, 2023
+ * Author: Frank Lin
+ * 
+ * This class is used to represent a gym class.
+ */
+package main.java.memoranda;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class GymClass {
+
+    private Date date;
+    
+}
