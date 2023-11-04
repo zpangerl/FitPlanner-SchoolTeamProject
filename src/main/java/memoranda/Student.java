8148759d07eb2b1804788bf85abcc61856437f4b
Sean <smckeig1@asu.edu>
2023-11-04 11:17:01 -0500

    belt_color data type update to BeltRank class

--- src/main/java/memoranda/Student.java
+++ src/main/java/memoranda/Student.java
@@ -3,25 +3,25 @@ package main.java.memoranda;
 public class Student {
     private String firstName;
     private String lastName;
-    private int belt_color;
-    private int training_rank;
+    private BeltRank belt_color;
+    private BeltRank training_rank;
 
     public String getFirstName() { return this.firstName; }
     public String getLastName() { return this.lastName; }
-    public int getBeltColor() { return this.belt_color; }
-    public int getTrainingRank() { return this.training_rank; }
+    public BeltRank getBeltColor() { return this.belt_color; }
+    public BeltRank getTrainingRank() { return this.training_rank; }
 
     public void setFirstName(String inputFirstName) { this.firstName = inputFirstName; }
     public void setLastName(String inputLastName) { this.lastName = inputLastName; }
-    public void setBeltColor(int inputColor) { this.belt_color = inputColor; }
-    public void setTrainingRank(int inputRank) { this.training_rank = inputRank; }
+    public void setBeltColor(BeltRank inputColor) { this.belt_color = inputColor; }
+    public void setTrainingRank(BeltRank inputRank) { this.training_rank = inputRank; }
 
     public Student(String inputLastName, String inputFirstName){
         this.lastName = inputLastName;
         this.firstName = inputFirstName;
     }
 
-    public Student(String inputLastName, String inputFirstName, int inputBeltColor, int inputTrainingRank){
+    public Student(String inputLastName, String inputFirstName, BeltRank inputBeltColor, BeltRank inputTrainingRank){
         this.lastName = inputLastName;
         this.firstName = inputFirstName;
         this.belt_color = inputBeltColor;
