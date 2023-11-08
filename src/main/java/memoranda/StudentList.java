ecf837c0f4affa968a560a5cb1ed140b3deabe9c
Sean <smckeig1@asu.edu>
2023-11-08 05:01:05 -0600

    US79/Task129, Includes javadoc comments in StudentList and StudentListImpl classes

--- src/main/java/memoranda/StudentList.java
+++ src/main/java/memoranda/StudentList.java
@@ -5,8 +5,33 @@ import nu.xom.Document;
 import java.util.Vector;
 
 public interface StudentList {
+    /**
+     * Returns a Student object given the students first and last name.
+     *
+     * @param lastName Students last name.
+     * @param firstName Students first name.
+     * @return Student object.
+     */
     Student getStudent(String lastName, String firstName);
+
+    /**
+     * Adds a Student object to the StudentList
+     *
+     * @param student_add Student object to add.
+     */
     void addStudent(Student student_add);
+
+    /**
+     * Removes a Student object for the StudentList.
+     *
+     * @param student_remove Student object to remove.
+     */
     void removeStudent(Student student_remove);
+
+    /**
+     * Returns the current number of Students in the StudentList.
+     *
+     * @return Student count.
+     */
     int getAllStudentCount();
 }
--- src/main/java/memoranda/StudentListImpl.java
+++ src/main/java/memoranda/StudentListImpl.java
@@ -7,10 +7,24 @@ public class StudentListImpl implements StudentList {
 
     private ArrayList<Student> studentList = new ArrayList<Student>();
 
+    /**
+     * Checks if a Student object exists in the StudentList.
+     *
+     * @param check_student Student object to check for.
+     * @return Returns true if student is in list, false otherwise.
+     */
     public boolean studentExists(Student check_student){
         return studentList.contains(check_student);
     }
 
+    /**
+     * Returns Student object from StudentLlist given the students
+     * first and last name.
+     *
+     * @param lastName Students last name.
+     * @param firstName Students first name.
+     * @return Student object.
+     */
     @Override
     public Student getStudent(String lastName, String firstName) {
         for (int i = 0; i < studentList.size(); i++)
@@ -19,16 +33,31 @@ public class StudentListImpl implements StudentList {
         return null;
     }
 
+    /**
+     * Adds Student object to StudentList.
+     *
+     * @param student_add Student object to add.
+     */
     @Override
     public void addStudent(Student student_add) {
         studentList.add(student_add);
     }
 
+    /**
+     * Removes Student object from StudentList.
+     *
+     * @param student_remove Student object to remove.
+     */
     @Override
     public void removeStudent(Student student_remove) {
         studentList.remove(student_remove);
     }
 
+    /**
+     * Returns the current count of Students in StudentList.
+     *
+     * @return Count of current Students.
+     */
     @Override
     public int getAllStudentCount() {
         return studentList.size();
