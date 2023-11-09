71580f9bd3f91d9b36fff48aff16e2cb3eb8eb0f
Frank J L <67761168+frankjlin16@users.noreply.github.com>
2023-11-09 01:15:01 -0700

    US83 Task99, Add UI form for creating new gym class. (WIP)
    Form options still looks like the form for creating a new task.

---
+++ src/main/java/memoranda/ui/GymClassDialog.java
@@ -0,0 +1,286 @@
+package main.java.memoranda.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import main.java.memoranda.date.CalendarDate;
+import main.java.memoranda.util.Local;
+
+public class GymClassDialog extends JDialog {
+
+    Border titledBorder;
+    JPanel mPanel = new JPanel(new BorderLayout());
+    JPanel areaPanel = new JPanel(new BorderLayout());
+    JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    JButton cancelB = new JButton();
+    JButton okB = new JButton();
+    Border border1;
+    Border border2;
+    JPanel dialogTitlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JLabel header = new JLabel();
+    public boolean CANCELLED = true;
+    JPanel jPanel8 = new JPanel(new GridBagLayout());
+    Border border4;
+    JPanel jPanel2 = new JPanel(new GridLayout(3, 2));
+    JTextField todoField = new JTextField();
+
+    // added by rawsushi
+    JTextField effortField = new JTextField();
+    JTextArea descriptionField = new JTextArea();
+    JScrollPane descriptionScrollPane = new JScrollPane(descriptionField);
+
+    // Border border7;
+    Border border8;
+    CalendarFrame startCalFrame = new CalendarFrame();
+    CalendarFrame endCalFrame = new CalendarFrame();
+    String[] priority = { Local.getString("Lowest"), Local.getString("Low"),
+            Local.getString("Normal"), Local.getString("High"),
+            Local.getString("Highest") };
+    boolean ignoreStartChanged = false;
+    boolean ignoreEndChanged = false;
+    JPanel jPanel4 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    JPanel jPanel6 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JLabel jLabel6 = new JLabel();
+    JButton setStartDateB = new JButton();
+    JPanel jPanel1 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    JLabel jLabel2 = new JLabel();
+    JSpinner startDate;
+    JSpinner endDate;
+    // JSpinner endDate = new JSpinner(new SpinnerDateModel());
+    JButton setEndDateB = new JButton();
+    // JPanel jPanel3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel jPanel3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel jPanelEffort = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    // JPanel jPanelNotes = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+    JButton setNotifB = new JButton();
+    JComboBox<String> priorityCB = new JComboBox<>(priority);
+    JLabel jLabel7 = new JLabel();
+    // added by rawsushi
+    JLabel jLabelEffort = new JLabel();
+    JLabel jLabelDescription = new JLabel();
+    JCheckBox chkEndDate = new JCheckBox();
+
+    JPanel jPanelProgress = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    JLabel jLabelProgress = new JLabel();
+    JSpinner progress = new JSpinner(new SpinnerNumberModel(0, 0, 100, 5));
+
+    public GymClassDialog(Frame frame, String title) {
+        super(frame, title, true);
+        try {
+            jbInit();
+            pack();
+        } catch (Exception e) {
+            new ExceptionDialog(e);
+        }
+    }
+
+    void jbInit() throws Exception {
+        this.setResizable(false);
+        this.setSize(new Dimension(430, 300));
+        titledBorder = new TitledBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0),
+                Local.getString("New Class"), TitledBorder.LEFT, TitledBorder.BELOW_TOP);
+
+        border1 = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        border2 = BorderFactory.createEtchedBorder(Color.white,
+                new Color(142, 142, 142));
+
+        border4 = BorderFactory.createEmptyBorder(0, 5, 0, 5);
+//        border5 = BorderFactory.createEmptyBorder();
+//        border6 = BorderFactory.createBevelBorder(BevelBorder.LOWERED,
+//            Color.white, Color.white, new Color(178, 178, 178),
+//            new Color(124, 124, 124));
+//        border7 = BorderFactory.createLineBorder(Color.white, 2);
+        border8 = BorderFactory.createEtchedBorder(Color.white, 
+            new Color(178, 178, 178));
+        cancelB.setMaximumSize(new Dimension(100, 26));
+        cancelB.setMinimumSize(new Dimension(100, 26));
+        cancelB.setPreferredSize(new Dimension(100, 26));
+        cancelB.setText(Local.getString("Cancel"));
+        cancelB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // cancelB_actionPerformed(e);
+                System.out.println("Cancel button pressed.");
+            }
+        });
+
+        startDate = new JSpinner(new SpinnerDateModel(new Date(),null,null,Calendar.DAY_OF_WEEK));
+        endDate = new JSpinner(new SpinnerDateModel(new Date(),null,null,Calendar.DAY_OF_WEEK));
+		
+        chkEndDate.setSelected(false);
+        okB.setMaximumSize(new Dimension(100, 26));
+        okB.setMinimumSize(new Dimension(100, 26));
+        okB.setPreferredSize(new Dimension(100, 26));
+        okB.setText(Local.getString("Ok"));
+        okB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // okB_actionPerformed(e);
+                System.out.println("Okay button pressed.");
+            }
+        });
+        
+        this.getRootPane().setDefaultButton(okB);
+        mPanel.setBorder(border1);
+        areaPanel.setBorder(border2);
+        dialogTitlePanel.setBackground(Color.WHITE);
+        dialogTitlePanel.setBorder(border4);
+        //dialogTitlePanel.setMinimumSize(new Dimension(159, 52));
+        //dialogTitlePanel.setPreferredSize(new Dimension(159, 52));
+        header.setFont(new java.awt.Font("Dialog", 0, 20));
+        header.setForeground(new Color(0, 0, 124));
+        header.setText(Local.getString("To do"));
+        header.setIcon(new ImageIcon(main.java.memoranda.ui.TaskDialog.class.getResource(
+            "/ui/icons/task48.png")));
+        
+        GridBagLayout gbLayout = (GridBagLayout) jPanel8.getLayout();
+        jPanel8.setBorder(titledBorder);
+				
+        todoField.setBorder(border8);
+        todoField.setPreferredSize(new Dimension(375, 24));
+        GridBagConstraints gbCon = new GridBagConstraints();
+        gbCon.gridwidth = GridBagConstraints.REMAINDER;
+        gbCon.weighty = 1;
+        gbLayout.setConstraints(todoField,gbCon);
+        
+        jLabelDescription.setMaximumSize(new Dimension(100, 16));
+        jLabelDescription.setMinimumSize(new Dimension(60, 16));
+        jLabelDescription.setText(Local.getString("Description"));
+        gbCon = new GridBagConstraints();
+        gbCon.gridwidth = GridBagConstraints.REMAINDER;
+        gbCon.weighty = 1;
+        gbCon.anchor = GridBagConstraints.WEST;
+        gbLayout.setConstraints(jLabelDescription,gbCon);
+
+        descriptionField.setBorder(border8);
+        descriptionField.setPreferredSize(new Dimension(375, 387)); // 3 additional pixels from 384 so that the last line is not cut off
+        descriptionField.setLineWrap(true);
+        descriptionField.setWrapStyleWord(true);
+        gbCon = new GridBagConstraints();
+        gbCon.gridwidth = GridBagConstraints.REMAINDER;
+        gbCon.weighty = 3;
+        descriptionScrollPane.setPreferredSize(new Dimension(375,96));
+        gbLayout.setConstraints(descriptionScrollPane,gbCon);
+
+        jLabelEffort.setMaximumSize(new Dimension(100, 16));
+        jLabelEffort.setMinimumSize(new Dimension(60, 16));
+        jLabelEffort.setText(Local.getString("Est Effort(hrs)"));
+        effortField.setBorder(border8);
+        effortField.setPreferredSize(new Dimension(30, 24));
+
+        startDate.setBorder(border8);
+        startDate.setPreferredSize(new Dimension(80, 24));                
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		sdf = (SimpleDateFormat)DateFormat.getDateInstance(DateFormat.SHORT);
+		// //Added by (jcscoobyrs) on 14-Nov-2003 at 10:45:16 PM
+		startDate.setEditor(new JSpinner.DateEditor(startDate, sdf.toPattern()));
+
+
+        jLabel6.setText(Local.getString("Start date"));
+        //jLabel6.setPreferredSize(new Dimension(60, 16));
+        jLabel6.setMinimumSize(new Dimension(60, 16));
+        jLabel6.setMaximumSize(new Dimension(100, 16));
+        setStartDateB.setMinimumSize(new Dimension(24, 24));
+        setStartDateB.setPreferredSize(new Dimension(24, 24));
+        setStartDateB.setText("");
+        setStartDateB.setIcon(
+            new ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource("/ui/icons/calendar.png")));
+       
+        jLabel2.setMaximumSize(new Dimension(270, 16));
+        //jLabel2.setPreferredSize(new Dimension(60, 16));
+        jLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+        jLabel2.setText(Local.getString("End date"));
+        endDate.setBorder(border8);
+        endDate.setPreferredSize(new Dimension(80, 24));
+        
+		endDate.setEditor(new JSpinner.DateEditor(endDate, sdf.toPattern())); //Added by (jcscoobyrs) on
+		//14-Nov-2003 at 10:45:16PM
+        
+        setEndDateB.setMinimumSize(new Dimension(24, 24));
+        setEndDateB.setPreferredSize(new Dimension(24, 24));
+        setEndDateB.setText("");
+        setEndDateB.setIcon(
+            new ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource("/ui/icons/calendar.png")));
+        
+        setNotifB.setText(Local.getString("Set notification"));
+        setNotifB.setIcon(
+            new ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource("/ui/icons/notify.png")));
+        jLabel7.setMaximumSize(new Dimension(100, 16));
+        jLabel7.setMinimumSize(new Dimension(60, 16));
+        //jLabel7.setPreferredSize(new Dimension(60, 16));
+        jLabel7.setText(Local.getString("Priority"));
+
+        priorityCB.setFont(new java.awt.Font("Dialog", 0, 11));
+        jPanel4.add(jLabel7, null);
+        getContentPane().add(mPanel);
+        mPanel.add(areaPanel, BorderLayout.CENTER);
+        mPanel.add(buttonsPanel, BorderLayout.SOUTH);
+        buttonsPanel.add(okB, null);
+        buttonsPanel.add(cancelB, null);
+        this.getContentPane().add(dialogTitlePanel, BorderLayout.NORTH);
+        dialogTitlePanel.add(header, null);
+        areaPanel.add(jPanel8, BorderLayout.NORTH);
+        jPanel8.add(todoField, null);
+        jPanel8.add(jLabelDescription);
+        jPanel8.add(descriptionScrollPane, null);
+        areaPanel.add(jPanel2, BorderLayout.CENTER);
+        jPanel2.add(jPanel6, null);
+        jPanel6.add(jLabel6, null);
+        jPanel6.add(startDate, null);
+        jPanel6.add(setStartDateB, null);
+        jPanel2.add(jPanel1, null);
+		jPanel1.add(chkEndDate, null);
+        jPanel1.add(jLabel2, null);
+        jPanel1.add(endDate, null);
+        jPanel1.add(setEndDateB, null);
+        // added by rawsushi
+        jPanel2.add(jPanelEffort, null);
+        jPanelEffort.add(jLabelEffort, null);
+        jPanelEffort.add(effortField, null);
+
+        jPanel2.add(jPanel4, null);
+        jPanel4.add(priorityCB, null);
+        jPanel2.add(jPanel3, null);
+        
+        jPanel3.add(setNotifB, null);
+        
+        jLabelProgress.setText(Local.getString("Progress"));
+        jPanelProgress.add(jLabelProgress, null);
+        jPanelProgress.add(progress, null);
+        jPanel2.add(jPanelProgress);
+        
+        priorityCB.setSelectedItem(Local.getString("Normal"));
+
+    }
+
+}
