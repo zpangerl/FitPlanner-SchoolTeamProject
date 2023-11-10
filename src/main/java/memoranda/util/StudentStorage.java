8308d738783f607ae10b97781f861362cf352c01
Sean Mckeighan <smckeig1@asu.edu>
2023-11-10 11:45:47 -0600

    US133/Task137, Initial StudentStorage class, WIP

---
+++ src/main/java/memoranda/util/StudentStorage.java
@@ -0,0 +1,19 @@
+package main.java.memoranda.util;
+
+import javax.swing.text.html.parser.Element;
+import java.util.ArrayList;
+
+public class StudentStorage {
+
+    public StudentStorage() {
+
+    }
+
+    public ArrayList<Element> readFile() {
+        return null;
+    }
+
+    public void writeFile(ArrayList<Element> studentList) {
+
+    }
+}
