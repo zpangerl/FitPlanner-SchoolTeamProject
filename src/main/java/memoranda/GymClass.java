287d2ad0dd429f8a2a327eed03626d948a6cb3e2
zpangerl <147574299+zpangerl@users.noreply.github.com>
2023-11-12 06:17:57 -0700

    US84 Task120: Add room attribute to GymClass.java (#37)
    * US83-T129: Add GymClass.java
    
    * US83-T129: Implement getter and setter for GymClass.java
    
    * US84 Task120: Add room attribute to GymClass.java
    
    * Update gradle wrapper: `./gradlew`
    
    * Remove incorrect package declaration
    
    * Update `GymClass`
    
    Added room variable and the getter and setter for it.
    
    * US84 Task120: Add extra line to EOF
    
    Add an extra line to EOF of GymClass.java as asked to by Rhett
    
    ---------
    
    Co-authored-by: Frankie J L <67761168+frankjlin16@users.noreply.github.com>

---
+++ src/main/java/memoranda/GymClass.java
@@ -0,0 +1,80 @@
+/**
+ * GymClass.java
+ * Created: Nov. 2nd, 2023
+ * Author: Frank Lin
+ * 
+ * This class is used to represent a gym class.
+ */
+package main.java.memoranda;
+
+import java.util.Date;
+
+public class GymClass {
+
+    private Date date;
+    private Room room;
+    private Trainer trainer;
+    private ClassType classType;
+
+    /**
+     * Constructor for GymClass
+     * 
+     * @param date,    the date for the planned class.
+     * @param trainer, the trainer assigned to teach the class.
+     */
+    public GymClass(Date d, Trainer t) {
+        date = d;
+        trainer = t;
+    }
+
+    public void setClassType(String type) {
+        this.classType = new ClassType(type);
+    }
+
+    public String getClassType() {
+        return classType.toString();
+    }
+
+    public void setTrainer(Trainer t) {
+        trainer = t;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    public void setDate(Date d) {
+        date = d;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setRoom(Room r) {
+        room = r;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    /**
+     * Declares what type the class is.
+     */
+    public class ClassType {
+
+        String type;
+
+        /*
+         * ClassType Contructor
+         */
+        public ClassType(String type) {
+            this.type = type;
+        }
+
+        public String toString() {
+            return type;
+        }
+    }
+}
